e6e744be321c3c391c232a9984fde5c1b8cefdc6
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-10-04 16:39:00 +0100

    Basic math functions to script and add HTML tags

---
+++ script.js
@@ -0,0 +1,14 @@
+var tallyNum = 0;
+
+function add(numToAdd) {
+    tallyNum += numToAdd;
+}
+function subtract(numToRemove) {
+    tallyNum -= numToRemove;
+}
+function multiply(numToMultiply) {
+    tallyNum *= numToMultiply;
+}
+function divide(numToDivide) {
+    tallyNum /= numToDivide;
+}
