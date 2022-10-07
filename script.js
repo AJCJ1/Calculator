a9ad49ee89536c608170259a94805d39011ee5a6
Arnold Cubici-Jones <arnoldcubicijones@hotmail.co.uk>
2022-10-07 09:19:55 +0100

    Add basic calculator layout in HTML

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+//Mathematics
+
+
 var tallyNum = 0;
 
-function add(numToAdd) {
-    tallyNum += numToAdd;
+function add(numTally, numToAdd) {
+    numTally += numToAdd;
 }
-function subtract(numToRemove) {
-    tallyNum -= numToRemove;
+function subtract(numTally, numToRemove) {
+    numTally -= numToRemove;
 }
-function multiply(numToMultiply) {
-    tallyNum *= numToMultiply;
+function multiply(numTally, numToMultiply) {
+    numTally *= numToMultiply;
 }
-function divide(numToDivide) {
-    tallyNum /= numToDivide;
+function divide(numTally, numToDivide) {
+    numTally /= numToDivide;
+}
+function operator(operator, num1, num2) {
+    if (operator == '+') {
+        add(num1, num2);
+    } else if (operator == '-') {
+        subtract(num1, num2);
+    } else if (operator == '/') {
+        divide(num1, num2);
+    } else if (operator == '*') {
+        multiply(num1, num2);
+    }
 }
